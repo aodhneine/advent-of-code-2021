8939cbe93d422c7b249e107b6020e8f50e43b1c9
Aodhnait Étain <aodhneine@pm.me>
2021-12-02 17:19:31 -0800

    perf: use O(log n) time comparison instead
    Replaces cond, which would take O(n) time for n cases, with O(log n),
    using case syntax. This cuts down time from 3000 comparisons for the
    input file down to only 1000.
    
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

--- day2/part1.rkt
+++ day2/part1.rkt
@@ -12,10 +12,10 @@
             [value (cdr current)]
             [distance (first accumulator)]
             [depth (second accumulator)])
-        (cond
-          [(eq? instruction 'forward) (list (+ distance value) depth)]
-          [(eq? instruction 'up) (list distance (- depth value))]
-          [(eq? instruction 'down) (list distance (+ depth value))])))
+        (case instruction
+          ['forward (list (+ distance value) depth)]
+          ['up (list distance (- depth value))]
+          ['down (list distance (+ depth value))])))
     (list 0 0) ; distance, depth
     instructions))
 
--- day2/part2.rkt
+++ day2/part2.rkt
@@ -13,10 +13,10 @@
             [distance (first accumulator)]
             [depth (second accumulator)]
             [aim (third accumulator)])
-        (cond
-          [(eq? instruction 'forward) (list (+ distance value) (+ depth (* aim value)) aim)]
-          [(eq? instruction 'up) (list distance depth (- aim value))]
-          [(eq? instruction 'down) (list distance depth (+ aim value))])))
+        (case instruction
+          ['forward (list (+ distance value) (+ depth (* aim value)) aim)]
+          ['up (list distance depth (- aim value))]
+          ['down (list distance depth (+ aim value))])))
     (list 0 0 0) ; distance, depth, aim
     instructions))
 
