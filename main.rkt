e300ca829dd1e6843e6e79646f93c6154426c191
Aodhnait Étain <aodhneine@pm.me>
2021-12-02 16:13:36 -0800

    refactor: change prefix style to more verbose
    We're now using a 'day_/part_' prefix, which is more readable. We also
    make the code little bit nicer by splitting import declarations into
    multiple lines.
    
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

--- main.rkt
+++ main.rkt
@@ -1,6 +1,9 @@
 #lang racket/base
 
-(require (prefix-in day2p2: "day2/part2.rkt") (prefix-in day2p1: "day2/part1.rkt") (prefix-in day2: "day2/parse.rkt"))
+(require
+  (prefix-in day2/part2: "day2/part2.rkt")
+  (prefix-in day2/part1: "day2/part1.rkt")
+  (prefix-in day2: "day2/parse.rkt"))
 
-(day2p1:compute (day2:parse "day2/input.txt"))
-(day2p2:compute (day2:parse "day2/input.txt"))
+(day2/part1:compute (day2:parse "day2/input.txt"))
+(day2/part2:compute (day2:parse "day2/input.txt"))
