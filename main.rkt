f619afbcd6628594ddc3abc5c9a57951522a5cc7
Aodhnait Étain <aodhneine@pm.me>
2021-12-02 16:12:50 -0800

    refactor: rename part1-input.txt to input.txt
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

--- main.rkt
+++ main.rkt
@@ -2,5 +2,5 @@
 
 (require (prefix-in day2p2: "day2/part2.rkt") (prefix-in day2p1: "day2/part1.rkt") (prefix-in day2: "day2/parse.rkt"))
 
-(day2p1:compute (day2:parse "day2/part1-input.txt"))
-(day2p2:compute (day2:parse "day2/part1-input.txt"))
+(day2p1:compute (day2:parse "day2/input.txt"))
+(day2p2:compute (day2:parse "day2/input.txt"))
